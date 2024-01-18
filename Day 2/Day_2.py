269e178e178b3c2a582e145634b2f68f6e754110
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-18 23:45:27 +0530

    Update Day_2.py

--- Day 2/Day_2.py
+++ Day 2/Day_2.py
@@ -19,5 +19,25 @@ second_number = int(two_digit_number[1])
 sum = first_number + second_number
 print(sum)
 
+# Lesson 6 Day 2 - BMI Calculator
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+height_float = float(height)
+bmi = int(float(weight) / (height_float*height_float))
+print(bmi)
+
+# Lesson 7 Day 2 - LIFE IN WEEKS
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+exact_age_left = 90 - int(age)
+no_of_weeks_in_an_year = 52
+total_weeks_left = exact_age_left * no_of_weeks_in_an_year
+print(f'You have {total_weeks_left} weeks left.')
+
 # Day 2 Completed!
 # Happy Coding🧡🤍💚
