787cf0b2a9ec75d2379383bd9dfb9c80879515bd
Ratn Govindam <88680132+RG-7@users.noreply.github.com>
2024-01-18 22:58:23 +0530

    Create Day_2.py and Completed!

---
+++ Day 2/Day_2.py
@@ -0,0 +1,23 @@
+# Day 2
+# January 18, 2024
+# Author: Ratn Govindam
+# #100DaysofCoding
+
+# Lesson 5 Day 2 - Data Types
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+sum = 0
+for ch in two_digit_number:
+  num = int(ch)
+  sum += num
+print(sum)
+# Or another code
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+sum = first_number + second_number
+print(sum)
+
+# Day 2 Completed!
+# Happy Coding🧡🤍💚
